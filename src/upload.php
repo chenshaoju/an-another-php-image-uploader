1402a86086e17b7ff180ace20cc987d3dad30f85
Chen Shaoju <oshirisu.red@gmail.com>
2023-02-04 23:34:05 +0800

    missing two /

--- src/upload.php
+++ src/upload.php
@@ -62,6 +62,6 @@ else {
 }
 
 echo $protocol . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']) . "/" . date('Y') . "/" . $filename . "." . $extension;
-echo '<br><br><img src="' . $protocol . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']) . "/" . date('Y') . "/" . $filename . "." . $extension . '" />';
+echo '<br /><br /><img src="' . $protocol . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']) . "/" . date('Y') . "/" . $filename . "." . $extension . '" />';
 
 ?>
